d9d8d9adc234450a6261063c7a345822251e47ae
Shafagh Sepehr <shafaghsepehr2003@gmail.com>
2023-04-13 23:10:23 +0330

    overloaded operator- for number class: number-int and int-number.  also the answer is always positive

--- 5- BIGcal/5- BIGcal.cpp
+++ 5- BIGcal/5- BIGcal.cpp
@@ -77,14 +77,14 @@ public:
 	}
 
 
-	number operator+(const number that) {
+	number operator+(const number that) const {
 
 		char* ans = sum(this->str, that.str);
 		number to_return(ans);
 		delete[] ans;
 		return to_return;
 	}
-	number operator+(long long int num) {
+	number operator+(long long num) const {
 
 		char* num_str = int_to_char(num);
 		char* ans = sum(this->str, num_str);
@@ -95,14 +95,8 @@ public:
 	}
 	static char* sum(char* a, char* b) {
 
-
 		int len1, len2, temp;
 		char* ans;
-
-
-
-
-
 		bool overflow = false;
 
 		len1 = strlen(a);
@@ -168,12 +162,142 @@ public:
 	}
 
 
+	number operator-(const number that) const {
+
+		char* ans = sub(this->str, that.str);
+		number to_return(ans);
+		delete[] ans;
+		return to_return;
+	}
+	number operator-(long long num) const {
+
+		char* num_str = int_to_char(num);
+		char* ans = sub(this->str, num_str);
+		number to_return(ans);
+		delete[] ans;
+		delete[] num_str;
+		return to_return;
+	}
+	static char* sub(char* a, char* b) {
+
+
+
+
+		int len1, len2, temp;
+
+		bool cond = false;
+
+		len1 = strlen(a);
+		len2 = strlen(b);
+
+		if (len2 > len1) {
+
+
+			char* temp = a;
+			a = b;
+			b = temp;
+			int tmp = len2;
+			len2 = len1;
+			len1 = tmp;
+		}
+		if (len2 == len1) {
+			for (int p = 0; p < len1; p++) {
+				if (a[p] == b[p]) {
+					continue;
+				}
+				else if (a[p] < b[p]) {
+					char* temp = a;
+					a = b;
+					b = temp;
+					int tmp = len2;
+					len2 = len1;
+					len1 = tmp;
+					break;
+				}
+				else
+					break;
+
+			}
+
+		}
+
+
+		char* ans = new char[len1 + 2];
+
+
+		if (!strcmp(a, b)) {
+			ans[0] = '0';
+			ans[1] = '\0';
+
+			return ans;
+		}
+		else {
+			ans[len1] = '\0';
+			for (int n = len1 - 1, m = len2 - 1; n >= 0; n--, m--) {
+				if (m >= 0) {
+					temp = a[n] - b[m];
+					if (cond) {
+						temp -= 1;
+						cond = false;
+					}
+					if (temp >= 0) {
+						ans[n] = temp + '0';
+					}
+					else {
+						cond = true;
+						temp += 10;
+						ans[n] = temp + '0';
+					}
+				}
+				else {
+					if (cond) {
+						temp = a[n] - 1 - '0';
+						cond = false;
+						if (temp < 0) {
+							cond = true;
+							temp += 10;
+
+						}
+						ans[n] = temp + '0';
+
+					}
+					else {
+						ans[n] = a[n];
+					}
+
+				}
+			}
+
+
+
+
+
+			ans[len1] = '\0';
+			while (ans[0] - '0' == 0) {
+				for (int k = 0; k < len1; k++) {
+					ans[k] = ans[k + 1];
+
+				}
+			}
+
+		}
+
+
+		return ans;
+	}
+
+
+
 
 	char* get_num_str() { return str; }
 };
 
-number operator+(long long num, number num_obj) {
+number operator+(long long num, const number num_obj) {
 	return num_obj + num;
+}
+
+number operator-(long long num, const number num_obj) {
+	return num_obj - num;
 }
 
 ostream& operator<<(ostream& out, number num_obj) {
@@ -195,7 +319,9 @@ int main() {
 	cout << 9 << " + " << a << " = " << d << endl;
 	cout << d << " + " << 9 << " = " << e << endl;
 
-
+	number f("10000"), g("5000");
+	cout << f << " - " << g << " = " << f - g << endl;
+	cout << g - f << endl;
 
 
 	return 0;
