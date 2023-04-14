ada78b23577b0dc29445df3f37654a3249e1420c
Shafagh Sepehr <shafaghsepehr2003@gmail.com>
2023-04-14 14:52:05 +0330

    overloaded operator<,>,<=,>=,-=,+=,*= added converting constructor for int(long long) operator (string) returns str in form of string commented all overloaded functions for int, cuz now converting constructor converts them to number class and functions for number+*-number work for them changed mul function to multiply numbers of any length

--- 5- BIGcal/5- BIGcal.cpp
+++ 5- BIGcal/5- BIGcal.cpp
@@ -1,10 +1,14 @@
 //chon str az gens pointer hast va faghat address on yeki str ro save mikone. baraye copy meghdar str-e vorodi bayad this.str ro new card va maghadir ro tosh copy kard
+
+
 #include <iostream>
 #include <limits.h>
 #pragma warning(disable:4996)
 using namespace std;
-#define SIZE 150
-#define MUL_MAX_SIZE SIZE*2
+
+
+#define SIZE 150//not so important, set it to 0 and nothing'l go wrong
+#define MUL_MAX_SIZE SIZE
 
 int get_int_len(long long int num) {
 	int len = 0;
@@ -15,7 +19,7 @@ int get_int_len(long long int num) {
 	return len;
 }
 
-char* int_to_char(long long int num) {
+char* int_to_char(long long int num) {//you sould manually free the returned pointer's memory after use
 	int len = get_int_len(num);
 	char* str = new char[len + 2];
 	for (int i = 0; i < len; i++) {
@@ -34,7 +38,7 @@ private:
 	int size;
 public:
 	//constructor 1
-	number() :str(new char[SIZE]), size(0) { str[0] = '0'; str[1] = '\0'; }
+	number() :str(new char[SIZE + 1]), size(0) { str[0] = '0'; str[1] = '\0'; }
 
 	//constructor 2
 	number(const char* s, int l) {
@@ -46,21 +50,30 @@ public:
 			exit(0);
 		}
 
-		str = new char[max(SIZE, l)];
+		str = new char[max(SIZE, l) + 1];
 		strcpy(str, s);
 
 	}
 
-	//constructor 3
+	//converting constructor
 	number(const char* s) {
-		str = new char[max((int)strlen(s), SIZE)];
+		str = new char[max((int)strlen(s), SIZE) + 1];
 		strcpy(str, s);
 		size = strlen(str);
 	}
 
+	//converting constructor
+	number(long long num) {
+		size = get_int_len(num);
+		str = new char[SIZE + 1];
+		char* num_str = int_to_char(num);
+		strcpy(str, num_str);
+		delete[] num_str;
+	}
+
 	//copy constructor
 	number(const number& that) {
-		str = new char[max(that.size, SIZE)];
+		str = new char[max(that.size, SIZE) + 1];
 		size = that.size;
 		strcpy(this->str, that.str);
 	}
@@ -71,6 +84,9 @@ public:
 	//copy(=)
 	number operator=(const number& that) {
 		size = that.size;
+		if (size > SIZE) {
+			str = new char[size + 1];
+		}
 		strcpy(this->str, that.str);
 		return *this;
 	}
@@ -92,6 +108,8 @@ public:
 	}
 
 
+	operator string() { return string(str); }
+
 	number operator+(const number that) const {
 
 		char* ans = sum(this->str, that.str);
@@ -99,16 +117,6 @@ public:
 		delete[] ans;
 		return to_return;
 	}
-	number operator+(long long num) const {
-
-		char* num_str = int_to_char(num);
-		char* ans = sum(this->str, num_str);
-		number to_return(ans);
-		delete[] ans;
-		delete[] num_str;
-		return to_return;
-	}
-
 
 
 	number operator-(const number that) const {
@@ -118,15 +126,7 @@ public:
 		delete[] ans;
 		return to_return;
 	}
-	number operator-(long long num) const {
 
-		char* num_str = int_to_char(num);
-		char* ans = sub(this->str, num_str);
-		number to_return(ans);
-		delete[] ans;
-		delete[] num_str;
-		return to_return;
-	}
 
 	number operator*(const number that) const {
 
@@ -135,22 +135,213 @@ public:
 		delete[] ans;
 		return to_return;
 	}
-	number operator*(long long num) const {
+
+
+	number& operator-=(const number that) {
+		(*this) = (*this) - that;
+		return *this;
+	}
+
+	number& operator+=(const number that) {
+		(*this) = (*this) + that;
+		return *this;
+	}
+
+	number& operator*=(const number that) {
+		(*this) = (*this) * that;
+		return *this;
+	}
+
+
+	int operator<=(const number that)const {
+		if (this->size < that.size)
+			return 1;
+		else if (this->size > that.size)
+			return 0;
+		else
+			for (int i = 0; i < size; i++)
+				if ((*this)[i] == that[i])
+					continue;
+				else if ((*this)[i] < that[i])
+					return 1;
+				else
+					return 0;
+		return 1;
+	}
+	int operator>=(const number that)const {
+		if (this->size < that.size)
+			return 0;
+		else if (this->size > that.size)
+			return 1;
+		else
+			for (int i = 0; i < size; i++)
+				if ((*this)[i] == that[i])
+					continue;
+				else if ((*this)[i] < that[i])
+					return 0;
+				else
+					return 1;
+		return 1;
+	}
+	int operator<(const number that)const {
+		if (this->size < that.size)
+			return 1;
+		else if (this->size > that.size)
+			return 0;
+		else
+			for (int i = 0; i < size; i++)
+				if ((*this)[i] == that[i])
+					continue;
+				else if ((*this)[i] < that[i])
+					return 1;
+				else
+					return 0;
+		return 0;
+	}
+	int operator>(const number that)const {
+		if (this->size < that.size)
+			return 0;
+		else if (this->size > that.size)
+			return 1;
+		else
+			for (int i = 0; i < size; i++)
+				if ((*this)[i] == that[i])
+					continue;
+				else if ((*this)[i] < that[i])
+					return 0;
+				else
+					return 1;
+		return 0;
+	}
+
+
+	/*number operator+(long long num) const {
 
 		char* num_str = int_to_char(num);
-		char* ans = mul(this->str, num_str);
+		char* ans = sum(this->str, num_str);
 		number to_return(ans);
 		delete[] ans;
 		delete[] num_str;
 		return to_return;
-	}
+	}*/
+	;
+	/*number operator-(long long num) const {
+
+			char* num_str = int_to_char(num);
+			char* ans = sub(this->str, num_str);
+			number to_return(ans);
+			delete[] ans;
+			delete[] num_str;
+			return to_return;
+		}*/
+	;
+	/*number operator*(long long num) const {
+
+				char* num_str = int_to_char(num);
+				char* ans = mul(this->str, num_str);
+				number to_return(ans);
+				delete[] ans;
+				delete[] num_str;
+				return to_return;
+			}*/
+	;
+	/*int operator<=(long long num) const {
+				char* num_str = int_to_char(num);
+				int num_len = get_int_len(num);
+				int ans = -1;
+				if (this->size < num_len) {
+					ans = 1;
+				}
+				else if (this->size > num_len)
+					ans = 0;
+				else
+					for (int i = 0; i < size; i++)
+						if ((*this)[i] == num_str[i])
+							continue;
+						else if ((*this)[i] < num_str[i])
+							ans = 1;
+						else
+							ans = 0;
+				if (ans == -1)
+					ans = 1;
+				delete[] num_str;
+				return ans;
+			}*/
+	;
+	/*int operator>=(long long num) const {
+				char* num_str = int_to_char(num);
+				int num_len = get_int_len(num);
+				int ans = -1;
+				if (this->size < num_len)
+					ans = 0;
+				else if (this->size > num_len)
+					ans = 1;
+				else
+					for (int i = 0; i < size; i++)
+						if ((*this)[i] == num_str[i])
+							continue;
+						else if ((*this)[i] < num_str[i])
+							ans = 0;
+						else
+							ans = 1;
+				if (ans == -1)
+					ans = 1;
+				delete[] num_str;
+				return ans;
+			}*/
+	;
+	/*int operator<(long long num) const {
+				char* num_str = int_to_char(num);
+				int num_len = get_int_len(num);
+				int ans = -1;
+				if (this->size < num_len)
+					ans = 1;
+				else if (this->size > num_len)
+					ans = 0;
+				else
+					for (int i = 0; i < size; i++)
+						if ((*this)[i] == num_str[i])
+							continue;
+						else if ((*this)[i] < num_str[i])
+							ans = 1;
+						else
+							ans = 0;
+				if (ans == -1)
+					ans = 0;
+				delete[] num_str;
+				return ans;
+			}*/
+	;
+	/*int operator>(long long num) const {
+				char* num_str = int_to_char(num);
+				int num_len = get_int_len(num);
+				int ans = -1;
+				if (this->size < num_len)
+					ans = 0;
+				else if (this->size > num_len)
+					ans = 1;
+				else
+					for (int i = 0; i < size; i++)
+						if ((*this)[i] == num_str[i])
+							continue;
+						else if ((*this)[i] < num_str[i])
+							ans = 0;
+						else
+							ans = 1;
+				if (ans == -1)
+					ans = 0;
+				delete[] num_str;
+				return ans;
+			}*/
+
+
 
 	char* get_num_str() { return str; }
 
 
 
 
-
+	//you sould manually free the returned pointer's memory for bellow methods after use
 	static char* sub(char* a, char* b) {
 
 
@@ -326,16 +517,7 @@ public:
 
 	}
 	static char* mul(char* a, char* b) {
-
-		char* tempstr;
-		char* ans = new char[SIZE];
-
-		char* cp;
-
-		ans[0] = '0';
-		ans[0] = '\0';
-		int len1, len2, temp, cIndex = SIZE - 2, ctr0 = 0;
-		short strg = 0;
+		int len1, len2, temp, cIndex, ctr0 = 0, new_size;
 
 		len1 = strlen(a);
 		len2 = strlen(b);
@@ -348,13 +530,27 @@ public:
 			len2 = len1;
 			len1 = tmp;
 		}
+		new_size = max(len1 * 2 + 5, MUL_MAX_SIZE);
+
+
+		char* tempstr;
+		char* ans = new char[new_size];
+
+		char* cp;
+
+		ans[0] = '0';
+		ans[0] = '\0';
+
+		short strg = 0;
+
+
 
 
 		for (int n = len2 - 1; n >= 0; n--, ctr0++) {//b[n]
 
-			cIndex = SIZE - 2;
-			cp = new char[SIZE]();
-			tempstr = new char[SIZE + 1]();
+			cIndex = new_size - 2;
+			cp = new char[new_size]();
+			tempstr = new char[new_size + 1]();
 			if (ctr0)strcpy(cp, ans);
 
 
@@ -415,16 +611,14 @@ public:
 	}
 };
 
-number operator+(long long num, const number num_obj) {
-	return num_obj + num;
-}
+number operator+(long long num, const number num_obj) { return num_obj + num; }
+number operator-(long long num, const number num_obj) { return num_obj - num; }
+number operator*(long long num, const number num_obj) { return num_obj * num; }
+int operator<=(long long num, const number num_obj) { return num_obj >= num; }
+int operator>=(long long num, const number num_obj) { return num_obj <= num; }
+int operator<(long long num, const number num_obj) { return num_obj > num; }
+int operator>(long long num, const number num_obj) { return num_obj < num; }
 
-number operator-(long long num, const number num_obj) {
-	return num_obj - num;
-}
-number operator*(long long num, const number num_obj) {
-	return num_obj * num;
-}
 
 ostream& operator<<(ostream& out, number num_obj) {
 	out << num_obj.get_num_str();
@@ -451,10 +645,34 @@ int main() {
 
 
 	number h("11378975456"), i("1324679850");
-	cout << h << " * " << i << " = " << h * i << endl;
+	cout << h << " * " << i << " = " << h * i << endl << endl;
 
+	number z1("5"), z2("555"), z3("545"), z4("555");
+	cout << (z1 < z2) << endl;
+	cout << (z1 > z2) << endl;
+	cout << (z2 < z4) << endl;
+	cout << (z2 <= z4) << endl;
+	cout << (z2 >= z4) << endl;
+	cout << (z2 > z3) << endl << endl;
 
+	cout << (z1 < 555) << endl;
+	cout << (5 > z2) << endl;
+	cout << (555 < z4) << endl;
+	cout << (z2 <= 555) << endl;
+	cout << (z2 >= 555) << endl;
+	cout << (555 > z3) << endl << endl;
 
+	number x(10), x1(5), x2(2);
+
+	(x -= x1) -= x2;
+	cout << x << endl << x1 << endl << x2 << endl << endl;
+
+	number y("50000000000000000000002");
+	cout << string(y) << endl << endl;
+
+	number f1("3348643512315"), f2("87897846551331"), f3("32132131654777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777776122222222222222222221254576456222222222234534313498745321321564"), f4("1321320654987645541326412312312333333333333333333333337777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777612222222222222222222125457645777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777761222222222222222222212545764533333333333333123123123123131233654984651245467943");
+
+	cout << (f3 *= f4 -= f1 += f2) << endl << endl;
 	return 0;
 }
 
